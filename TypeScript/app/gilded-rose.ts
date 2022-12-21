c9078ec0fb7d11717ec3dbbf3db7323aec34f42b
Andrew Cline <acline@precisionplanting.com>
2022-12-21 11:46:06 -0600

    feat(TypeScript): add conjured items

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -59,6 +59,20 @@ export class GildedRose {
         return item
 
       default:
+        if (item.name.includes('Conjured')) {
+          if (item.quality > 0) {
+            item.quality = item.quality - 2
+          }
+
+          item.sellIn = item.sellIn - 1;
+
+          if (item.sellIn < 0 && item.quality > 0) {
+            item.quality = item.quality - 4
+          }
+
+          return item
+        }
+
         if (item.quality > 0) {
           item.quality = item.quality - 1
         }
@@ -70,6 +84,7 @@ export class GildedRose {
         }
 
         return item
+
     }
 
   }
--- TypeScript/test/jest/gilded-rose.spec.ts
+++ TypeScript/test/jest/gilded-rose.spec.ts
@@ -18,7 +18,6 @@ Sulfuras, Hand of Ragnaros -1 80
 Backstage passes to a TAFKAL80ETC concert 15 20
 Backstage passes to a TAFKAL80ETC concert 10 49
 Backstage passes to a TAFKAL80ETC concert 5 49
-Conjured Mana Cake 3 6
 -------- day 1 --------
 name, sellIn, quality
 +5 Dexterity Vest 9 19
@@ -29,7 +28,6 @@ Sulfuras, Hand of Ragnaros -1 80
 Backstage passes to a TAFKAL80ETC concert 14 21
 Backstage passes to a TAFKAL80ETC concert 9 50
 Backstage passes to a TAFKAL80ETC concert 4 50
-Conjured Mana Cake 2 5
 `
 
     const items = [
@@ -41,8 +39,7 @@ Conjured Mana Cake 2 5
       new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
       new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
       new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-      // this conjured item does not work properly yet
-      new Item("Conjured Mana Cake", 3, 6)];
+    ];
 
 
     const gildedRose = new GildedRose(items);
@@ -64,5 +61,33 @@ Conjured Mana Cake 2 5
     }
 
     expect(result).toEqual(expected)
+  })
+
+  it('should be able to add a conjured item', () => {
+    const items = [new Item("Conjured Mana Cake", 3, 6)]
+
+    const gildedRose = new GildedRose(items);
+
+    expect(gildedRose.items).toEqual(expect.arrayContaining(items))
+
+    gildedRose.updateQuality()
+
+    expect(gildedRose.items).toEqual(
+      expect.arrayContaining([new Item('Conjured Mana Cake', 2, 4)])
+    )
+  })
+
+  it('should be able to add a different conjured item', () => {
+    const items = [new Item("Conjured sword", 10, 70)]
+
+    const gildedRose = new GildedRose(items);
+
+    expect(gildedRose.items).toEqual(expect.arrayContaining(items))
+
+    gildedRose.updateQuality()
+
+    expect(gildedRose.items).toEqual(
+      expect.arrayContaining([new Item('Conjured sword', 9, 68)])
+    )
   })
 });
